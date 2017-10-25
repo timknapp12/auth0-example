f0a921473ff73ee468d410983d7066553efacc3b
timknapp12 <timknapp12@gmail.com>
2017-10-25 13:40:16 -0600

    end of project

--- server/index.js
+++ server/index.js
@@ -3,12 +3,53 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const session = require('express-session');
+const passport = require('passport');
+const Auth0Strategy = require('passport-auth0');
 
 const app = express();
 
+app.use(bodyParser.json());
+app.use(cors());
+app.use(session({
+    secret: process.env.SECRET,
+    resave: false,
+    saveUninitialized: true
+}))
+app.use(passport.initialize());
+app.use(passport.session());
 
+passport.use(new Auth0Strategy({
+    domain: process.env.AUTH_DOMAIN,
+    clientID: process.env.AUTH_CLIENT_ID,
+    clientSecret: process.env.AUTH_CLIENT_SECRET,
+    callbackURL: process.env.AUTH_CALLBACK
+}, function(accessToken, refreshToken, extraParams, profile, done) {
 
+    //db queries
 
+    done(null, profile)
+}))
 
+passport.serializeUser(function(profile, done) {
+    done(null, profile);
+}) 
 
-app.listen(process.env.PORT, () => console.log(`listening on port ${process.env.PORT}`))
+passport.deserializeUser(function(profile, done) {
+    done(null, profile);
+}) 
+
+app.get('/auth', passport.authenticate('auth0'));
+app.get('/auth/callback', passport.authenticate('auth0', {
+    successRedirect: 'http://localhost:3000/',
+    failureRedirect: '/auth'
+}))
+
+app.get('/auth/me', function(req, res) {
+    if (!req.user) {
+        return res.status(401).send('No user logged in');
+    }
+    return res.status(200).send(req.user);
+})
+
+const port = 3535;
+app.listen(port, () => console.log(`listening on port ${port}`))
--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
 class App extends Component {
+
+  handleClick() {
+    axios.get('/auth/me')
+    .then(function(res) {
+      console.log(res);
+    })
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+       <a href={process.env.REACT_APP_LOGIN}><button>Button</button></a>
+       <button onClick={this.handleClick}>Get User Info</button>
       </div>
     );
   }
